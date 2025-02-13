d4a0027c16601159e3cef94285bc4beff9174ea3
Kira <kafountain5@gmail.com>
2025-02-12 20:14:28 -0500

    ceiling colliders

---
+++ Assets/Code/Scripts/Door.cs
@@ -0,0 +1,28 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void Open()
+    {
+        transform.DORotate(new Vector3(0f, -180f, 0f), 1f);
+    }
+
+    public void Close()
+    {
+        transform.DORotate(new Vector3(0f, -90f, 0f), 1f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Open();
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            Close();
+        }
+    }
+}
