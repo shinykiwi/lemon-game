d37e6180c6b454b47523474d48b9e8c19e6f52a9
Kira <kafountain5@gmail.com>
2025-02-17 15:25:19 -0500

    fixed collisions maybe omg

--- Assets/Code/Scripts/Interactable.cs
+++ Assets/Code/Scripts/Interactable.cs
@@ -110,4 +110,9 @@ public class Interactable : MonoBehaviour
     {
         return canInteract;
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
 }
